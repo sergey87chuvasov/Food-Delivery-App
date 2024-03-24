b2401fd3403c0b7b97b3637dbb82319514124d34
Sergey87 <serge.techno@gmail.com>
2024-03-24 17:35:54 +0300

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favorite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque enim
+          deserunt tempore id impedit dolorum quo iusto ipsa quos aspernatur.
+          Rerum aliquid nostrum at quasi officiis sequi laboriosam eligendi
+          odit?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Header from '../../components/Header/Header';
 import './Home.css';
 
 function Home() {
-  return <div className='home'>Home PAGE</div>;
+  return (
+    <div className='home'>
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
