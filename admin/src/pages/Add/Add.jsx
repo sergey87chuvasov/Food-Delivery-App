0c44326df0ee121b48067b0e6d0b919d59385e3e
Sergey87 <serge.techno@gmail.com>
2024-05-10 00:08:30 +0300

    added Display food list in the admin panel

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -4,8 +4,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-function Add() {
-  const url = 'http://localhost:4000';
+function Add({ url }) {
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: '',
