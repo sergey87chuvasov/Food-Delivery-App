f6fcee02a580b510c2a94c474b103e246fc75ca2
Sergey87 <serge.techno@gmail.com>
2024-05-31 20:21:03 +0300

    Create Shop Cart functionality - 1 part

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config.js';
+import cartRouter from './routes/cartRoute.js';
 
 // app config - init our app use express
 const app = express();
@@ -19,7 +20,8 @@ connectDB();
 // api endpoint
 app.use('/api/food', foodRouter);
 app.use('/images', express.static('uploads')); // folder name // http://localhost:4000/images/1714939310824food_17.png
-app.use('/api/user', userRouter); //
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
 
 app.get('/', (req, res) => {
   res.send('API Work!');
