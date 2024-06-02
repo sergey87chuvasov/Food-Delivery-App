fef57397c6b8a3e7248217a32ce8a0595066f46c
Sergey87 <serge.techno@gmail.com>
2024-06-02 15:43:53 +0300

    Create place order feature and stripe payment integration - 2 part

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,16 +1,49 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './PlaceOrder.css';
 import { StoreContext } from '../../context/StoreContext';
 
 function PlaceOrder() {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    country: '',
+    phone: '',
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.value;
+    const value = event.target.value;
+
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
   return (
     <form className='place-order'>
       <div className='place-order-left'>
         <p className='title'>Delivery Information</p>
         <div className='multi-fields'>
-          <input type='text' placeholder='First Name' />
-          <input type='text' placeholder='Last Name' />
+          <input
+            name='firstName'
+            onChange={onChangeHandler}
+            value={data.firstName}
+            type='text'
+            placeholder='First Name'
+          />
+          <input
+            name='lastName'
+            onChange={onChangeHandler}
+            value={data.lastName}
+            type='text'
+            placeholder='Last Name'
+          />
         </div>
         <input type='email' placeholder='Email address' />
         <input type='text' placeholder='Street' />
