10135322cc83e5b6fdbc6f943e485e14dbcc2343
Sergey87 <serge.techno@gmail.com>
2024-06-04 22:30:11 +0300

    Create user order page - 2 part

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -35,10 +35,20 @@ function MyOrders() {
               <img src={assets.parcel_icon} alt='order icon pic' />
               <p>
                 {order.items.map((item, index) => {
-                  if (index === order.items.length) {
+                  if (index === order.items.length - 1) {
+                    return item.name + ' x ' + item.quantity;
+                  } else {
+                    return item.name + ' x ' + item.quantity + ', ';
                   }
                 })}
               </p>
+              <p>${order.amount}.00</p>
+              <p>Items: {order.items.length}</p>
+              <p>
+                <span>&#x25cf;</span>
+                <b>{order.status}</b>
+              </p>
+              <button>Track Order</button>
             </div>
           );
         })}
