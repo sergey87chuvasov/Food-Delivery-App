3d79bf5e62882c0a70eedd2e7c3bd79d41dbd48b
Sergey87 <serge.techno@gmail.com>
2024-06-05 00:05:02 +0300

    Display orders in admin panel - 2 part

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,7 +1,53 @@
+import { useState } from 'react';
 import './Orders.css';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { assets } from '../../assets/assets';
 
-function Orders() {
-  return <div className='orders'></div>;
+function Orders({ url }) {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + '/api/order/list');
+
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error('Error');
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className='order-list'>
+        {orders.map((order, index) => (
+          <div key={index} className='order-item'>
+            <img src={assets.parcel_icon} alt='item pic' />
+            <p className='order-item-food'>
+              {order.items.map((item, index) => {
+                if (index === order.items.length - 1) {
+                  return item.name + ' x ' + item.quantity;
+                } else {
+                  return item.name + ' x ' + item.quantity + ', ';
+                }
+              })}
+            </p>
+            <p className='order-item-name'>
+              {order.address.firstName + ' ' + order.address.lastName}
+              <p className='order-item-address'></p>
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
